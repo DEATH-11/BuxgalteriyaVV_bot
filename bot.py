ae38178073908d8bd968d97f17fd97121873d655
DEATH-11 <sattorovjasur713@gmail.com>
2026-09-24 15:11:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,12 +4,16 @@ import logging
 from aiogram import Dispatcher
 
 from app.bot_instance import bot
+from app.handlers import shartnoma as shartnoma_handlers
+from app.handlers import spetsifikatsiya as spets_handlers
 from app.handlers import start as start_handlers
 
 logging.basicConfig(level=logging.INFO)
 
 dp = Dispatcher()
 dp.include_router(start_handlers.router)
+dp.include_router(shartnoma_handlers.router)
+dp.include_router(spets_handlers.router)
 
 
 async def main():
