15bc7596e83261242b3d5f23f468f23f9dbc77c3
DEATH-11 <sattorovjasur713@gmail.com>
2026-09-25 15:46:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import logging
 from aiogram import Dispatcher
 
 from app.bot_instance import bot
+from app.database.base import init_db
+from app.handlers import admin as admin_handlers
+from app.handlers import drugs as drugs_handlers
 from app.handlers import shartnoma as shartnoma_handlers
 from app.handlers import spetsifikatsiya as spets_handlers
 from app.handlers import start as start_handlers
@@ -12,11 +15,14 @@ logging.basicConfig(level=logging.INFO)
 
 dp = Dispatcher()
 dp.include_router(start_handlers.router)
+dp.include_router(admin_handlers.router)
 dp.include_router(shartnoma_handlers.router)
 dp.include_router(spets_handlers.router)
+dp.include_router(drugs_handlers.router)
 
 
 async def main():
+    await init_db()
     await dp.start_polling(bot)
 
 
